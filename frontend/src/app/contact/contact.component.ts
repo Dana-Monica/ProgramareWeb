351a3c84e0ff24629ee48a2c01a984995db52b65
Monica <monicatanase96@yahoo.com>
2017-11-21 19:32:25 +0200

    changed contact

--- frontend/src/app/contact/contact.component.ts
+++ frontend/src/app/contact/contact.component.ts
@@ -10,9 +10,36 @@ import { Http, Headers, RequestOptions } from '@angular/http';
 })
 export class ContactComponent implements OnInit {
 
-  constructor(public router: Router,public http: Http) { }
+  model : any = {};
+  loading = false;
+
+  constructor(public router: Router, public http: Http) { }
 
   ngOnInit() {
+  }
+
+  contact(name, mail, phone, message) {
+
+    let body = JSON.stringify({name, mail, phone, message});
+    let headers = new Headers();
+    headers.append('Content-Type','application/json');
+
+    console.log(body);
+    this.http.post('http://localhost:8079/send/email', body, { headers: headers})
+      .subscribe(
+        response => {
+          console.log("Hello from Contact!");
+          localStorage.setItem('name',name);
+          localStorage.setItem('mail',mail);
+          localStorage.setItem('phone',phone);
+          localStorage.setItem('message',message);
+          this.router.navigate(['contact']);
+        },
+        error => {
+          alert(error.text());
+          console.log(error.text());
+        }
+      );
   }
 
 }
